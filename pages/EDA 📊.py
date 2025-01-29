e7c73260b88c329ca1f1593b26ef8552cd8ecb2a
Piotr Kapusta <piotrkapusta@Piotrs-MacBook-Air.local>
2025-01-29 22:35:14 +0100

    added df summary

--- pages/EDA 📊.py
+++ pages/EDA 📊.py
@@ -1,7 +1,10 @@
 import streamlit as st
+from function.utils import report_config
 from function.data_prep import get_churn_train_df
 from function.frontend import display_first_n_rows, display_columns_descriptions, display_df_summary
 
+
+report_config()
 
 st.header('EDA')
 df = get_churn_train_df()
@@ -10,7 +13,10 @@ left, right = st.columns(2)
 
 with left:
     display_first_n_rows(df, 10)
-    display_df_summary(df)
+    #display_df_summary(df)
 
 with right:
     display_columns_descriptions()
+
+
+
